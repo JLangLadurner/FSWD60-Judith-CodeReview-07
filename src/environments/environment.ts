31fcf5e605dbe624937bddd0a4dcd372ef3df108
Judith <judithmarialang@gmail.com>
2019-02-22 08:58:26 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+  	 apiKey: "AIzaSyDqtUdDS-pGWUb38FtwUMpnGso047tz8xk",
+    authDomain: "phonebookcr07.firebaseapp.com",
+    databaseURL: "https://phonebookcr07.firebaseio.com",
+    projectId: "phonebookcr07",
+    storageBucket: "phonebookcr07.appspot.com",
+    messagingSenderId: "496633286401"
+}
 };
 
 /*
